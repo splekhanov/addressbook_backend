50ff14d600bf8cf499270a8e613a9b7e40e07e4a
Flash <sergeyplehanov2014@yandex.ru>
2019-09-07 22:27:27 +0300

    One two

--- src/main/java/com/addressbook/configuration/Swagger2Config.java
+++ src/main/java/com/addressbook/configuration/Swagger2Config.java
@@ -2,6 +2,7 @@ package com.addressbook.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,6 +11,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@PropertySource("classpath:swagger.properties")
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
--- src/main/java/com/addressbook/controller/contact/ContactController.java
+++ src/main/java/com/addressbook/controller/contact/ContactController.java
@@ -2,6 +2,8 @@ package com.addressbook.controller.contact;
 
 import com.addressbook.dto.contact.ContactDTO;
 import com.addressbook.service.ContactService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
+@Api(tags = "Contact", value = "ContactCommands", description = "Controller for Contact Commands")
 public class ContactController {
 
     private final ContactService contactService;
@@ -21,6 +24,7 @@ public class ContactController {
         this.contactService = contactService;
     }
 
+    @ApiOperation(value = "Get contact by ID")
     @GetMapping(value = "/contacts/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<ContactDTO> getAccount(@PathVariable("id") Long id) {
         return ok(contactService.getContact(id));
