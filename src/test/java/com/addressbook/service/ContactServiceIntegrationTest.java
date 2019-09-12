c4a26fc0542110f5c830ebf5f8a539212f7908bf
Flash <sergeyplehanov2014@yandex.ru>
2019-09-12 19:21:01 +0300

    TEst fixes

--- src/test/java/com/addressbook/service/ContactServiceIntegrationTest.java
+++ src/test/java/com/addressbook/service/ContactServiceIntegrationTest.java
@@ -1,40 +1,14 @@
 package com.addressbook.service;
 
-import com.addressbook.controller.contact.ContactController;
-import com.addressbook.entity.contact.Contact;
-import com.addressbook.entity.customer.Customer;
-import com.addressbook.entity.security.User;
-import com.addressbook.repository.contact.ContactRepository;
-import com.addressbook.security.JwtAuthenticationEntryPoint;
-import com.addressbook.security.JwtAuthenticationFilter;
-import com.addressbook.service.impl.CustomUserDetailsService;
 import com.addressbook.utils.mapper.contact.ContactMapper;
-import com.sun.glass.ui.Application;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.Optional;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestPropertySource(
         locations = "classpath:application-integrationtest.properties")
