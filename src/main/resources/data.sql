9a0c4a1309c7808b6d632a49a97aea425f60ab0d
Flash <sergeyplehanov2014@yandex.ru>
2019-09-21 11:13:56 +0300

    SQL Script fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO user (id, name, password, enabled) VALUES (1, 'flash', '$2a$10$BEebJV2WCUPndkLvYiDbB.1MmvsdIvcdQIhD60mj/IXmy9QaUcBWy', true );
 INSERT INTO role (id, name) VALUES (1, 'admin');
--- INSERT INTO contact (id, first_name, last_name, phone, email, user_id) VALUES (1, 'Brad', 'Pitt', '+79811543120', 'bradpitt@yahoo.com', 1)
+INSERT INTO user (id, name, password, enabled) VALUES (1, 'flash', '$2a$10$BEebJV2WCUPndkLvYiDbB.1MmvsdIvcdQIhD60mj/IXmy9QaUcBWy', true );
+INSERT INTO contact (id, first_name, last_name, phone, email, user_id) VALUES (1, 'Brad', 'Pitt', '+79811543120', 'bradpitt@yahoo.com', 1)
